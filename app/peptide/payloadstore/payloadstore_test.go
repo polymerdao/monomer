12195b63c1702be163fd4ab8a8d9a9f7057a51d3
Josh Klopfenstein <git@joshklop.com>
2024-04-05 16:09:34 -0500

    Fix payloadstore tests
    Co-authored-by: Colin Kennedy <colin.kennedy@mesh.xyz>

--- app/peptide/payloadstore/payloadstore_test.go
+++ app/peptide/payloadstore/payloadstore_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 
 	"github.com/ethereum-optimism/optimism/op-service/eth"
+	"github.com/ethereum/go-ethereum/beacon/engine"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	"github.com/polymerdao/monomer"
@@ -17,29 +18,34 @@ func dummyPayload(height int64) *monomer.Payload {
 		GasLimit: &gaslimit,
 	}
 	hash := common.HexToHash(fmt.Sprintf("0x%x", height))
-	return eetypes.NewPayload(&attrs, hash, height)
+	return &monomer.Payload{
+		Timestamp:             uint64(attrs.Timestamp),
+		PrevRandao:            attrs.PrevRandao,
+		SuggestedFeeRecipient: attrs.SuggestedFeeRecipient,
+		Withdrawals:           attrs.Withdrawals,
+		NoTxPool:              attrs.NoTxPool,
+		GasLimit:              uint64(*attrs.GasLimit),
+		ParentBeaconBlockRoot: attrs.ParentBeaconBlockRoot,
+		ParentHash:            hash,
+		Height:                height,
+		Transactions:          attrs.Transactions,
+	}
 }
 
 func TestRollback(t *testing.T) {
 	ps := NewPayloadStore()
-	ids := make([]*eetypes.PayloadID, 10)
+	ids := make([]*engine.PayloadID, 10)
 
 	for h := int64(0); h < 10; h++ {
 		payload := dummyPayload(h)
-		require.NoError(t, ps.Add(payload))
-		id, err := payload.GetPayloadID()
-		require.NoError(t, err)
-		ids[h] = id
+		ps.Add(payload)
+		ids[h] = payload.ID()
 	}
 
 	for h := int64(0); h < 10; h++ {
 		newpayload, ok := ps.Get(*ids[h])
 		require.True(t, ok)
-
-		id, err := newpayload.GetPayloadID()
-		require.NoError(t, err)
-
-		require.Equal(t, ids[h], id)
+		require.Equal(t, ids[h], newpayload.ID())
 	}
 
 	require.NoError(t, ps.RollbackToHeight(5))
