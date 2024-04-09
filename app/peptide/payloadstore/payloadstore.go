73f0487f28d8f45098ae1ed4c7a615bfabd080eb
Colin Kennedy <colin.kennedy@mesh.xyz>
2024-04-09 12:33:54 -0300

    Remove redundant mutex
    Concurrency safety is handled at the engine level.

--- app/peptide/payloadstore/payloadstore.go
+++ app/peptide/payloadstore/payloadstore.go
@@ -1,8 +1,6 @@
 package payloadstore
 
 import (
-	"sync"
-
 	"github.com/ethereum/go-ethereum/beacon/engine"
 	"github.com/polymerdao/monomer"
 )
@@ -15,7 +13,6 @@ type PayloadStore interface {
 }
 
 type pstore struct {
-	mutex    sync.Mutex
 	payloads map[engine.PayloadID]*monomer.Payload
 	heights  map[int64]engine.PayloadID
 	current  *monomer.Payload
@@ -25,7 +22,6 @@ var _ PayloadStore = (*pstore)(nil)
 
 func NewPayloadStore() PayloadStore {
 	return &pstore{
-		mutex:    sync.Mutex{},
 		payloads: make(map[engine.PayloadID]*monomer.Payload),
 		heights:  make(map[int64]engine.PayloadID),
 	}
@@ -33,8 +29,6 @@ func NewPayloadStore() PayloadStore {
 
 func (p *pstore) Add(payload *monomer.Payload) {
 	id := payload.ID()
-	p.mutex.Lock()
-	defer p.mutex.Unlock()
 	if _, ok := p.payloads[*id]; !ok {
 		p.heights[payload.Height] = *id
 		p.payloads[*id] = payload
@@ -43,8 +37,6 @@ func (p *pstore) Add(payload *monomer.Payload) {
 }
 
 func (p *pstore) Get(id engine.PayloadID) (*monomer.Payload, bool) {
-	p.mutex.Lock()
-	defer p.mutex.Unlock()
 	if payload, ok := p.payloads[id]; ok {
 		return payload, true
 	}
@@ -52,15 +44,10 @@ func (p *pstore) Get(id engine.PayloadID) (*monomer.Payload, bool) {
 }
 
 func (p *pstore) Current() *monomer.Payload {
-	p.mutex.Lock()
-	defer p.mutex.Unlock()
 	return p.current
 }
 
 func (p *pstore) RollbackToHeight(height int64) error {
-	p.mutex.Lock()
-	defer p.mutex.Unlock()
-
 	// nuke everything in memory
 	p.current = nil
 	p.heights = make(map[int64]engine.PayloadID)
