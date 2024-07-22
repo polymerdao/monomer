77f9e769b293304cd869fd0659d4effac2bdbfae
phasewalk1 <phasewalk1@proton.me>
2024-07-22 14:20:16 -0700

    initialize a docusaurus site

---
+++ doc/docs/intro.md
@@ -0,0 +1,22 @@
+---
+sidebar_position: 1
+---
+
+# Overview
+
+Monomer uses the OP stack to make Cosmos SDK applications deployable as
+Ethereum rollups.
+
+:::warning
+Monomer is pre-production software and should be used with caution.
+:::
+
+## At a Glance
+From the [OP stack](https://specs.optimism.io/protocol/overview.html#components)'s perspective, Monomer replaces the default Ethereum
+compatible execution engine. From the [Cosmos application](https://docs.cosmos.network/v0.50/learn/intro/why-app-specific#what-are-application-specific-blockchains)'s perspective,
+Monomer replaces the CometBFT consensus layer.
+
+:::note[Architecture] 
+![Architecture](/img/architecture.png)
+:::
+
---
+++ doc/src/components/HomepageFeatures/index.tsx
@@ -0,0 +1,58 @@
+import clsx from 'clsx';
+import Heading from '@theme/Heading';
+import styles from './styles.module.css';
+
+type FeatureItem = {
+  title: string;
+  Svg: React.ComponentType<React.ComponentProps<'svg'>>;
+  description: JSX.Element;
+};
+
+const FeatureList: FeatureItem[] = [
+  {
+    title: 'Easy to Use 🚀',
+    Svg: require('@site/static/img/undraw_docusaurus_mountain.svg').default,
+    description: (
+      <>
+        Monomer was designed from the ground up to be easily leveraged, allowing Cosmos SDK developers to deploy their applications as L2s with ease; finally breaking tech silos and enabling cross-chain composability. Use our custom Cosmos SDK hook to get up and running quick. 
+      </>
+    ),
+  },
+  {
+    title: 'Powered by the OP Stack 💥',
+    Svg: require('@site/static/img/undraw_docusaurus_tree.svg').default,
+    description: (
+      <>
+      Monomer lets you focus on your application logic and acts as a middleware between your Cosmos SDK app and the Ethereum network. Monomer utilizes the OP stack to provide a seamless experience for developers.
+      </>
+    ),
+  },
+];
+
+function Feature({title, Svg, description}: FeatureItem) {
+  return (
+    <div className={clsx('col col--4')}>
+      <div className="text--center">
+        <Svg className={styles.featureSvg} role="img" />
+      </div>
+      <div className="text--center padding-horiz--md">
+        <Heading as="h3">{title}</Heading>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+}
+
+export default function HomepageFeatures(): JSX.Element {
+  return (
+    <section className={styles.features}>
+      <div className="container">
+        <div className="row">
+          {FeatureList.map((props, idx) => (
+            <Feature key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
