b8cc21d02fd9d7f99263e801a6f495516fb96565
phasewalk1 <phasewalk1@proton.me>
2024-07-31 11:25:25 -0700

    fix edit page links

--- doc/docusaurus.config.ts
+++ doc/docusaurus.config.ts
@@ -38,14 +38,14 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/polymerdao/monomer/tree/main/doc/',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+            'https://github.com/polymerdao/monomer/tree/main/doc/',
         },
         theme: {
           customCss: './src/css/custom.css',
