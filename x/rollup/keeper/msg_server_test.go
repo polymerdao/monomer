f79cdcda6ae2115e1bd5f486d69b105391fd5322
AnkushinDaniil <ankushin.daniil42@gmail.com>
2024-08-06 20:01:11 +0300

    Refactor TestValidateBasic

--- x/rollup/keeper/msg_server_test.go
+++ x/rollup/keeper/msg_server_test.go
@@ -16,46 +16,59 @@ func TestValidateBasic(t *testing.T) {
 	binary.BigEndian.PutUint64(validGasLimit, 100_000)
 
 	belowRangeGasLimit := make([]byte, 8)
-	binary.BigEndian.PutUint64(belowRangeGasLimit, 4999)
+	binary.BigEndian.PutUint64(belowRangeGasLimit, 4_999)
 
 	aboveRangeGasLimit := make([]byte, 8)
-	binary.BigEndian.PutUint64(belowRangeGasLimit, 9_223_372_036_854_775_808)
+	binary.BigEndian.PutUint64(aboveRangeGasLimit, 9_223_372_036_854_775_808)
 
-	// Valid Ethereum address and gas limit within the allowed range
-	validRequest := &rollupv1.InitiateWithdrawalRequest{
-		Target:   validAddress,
-		GasLimit: validGasLimit,
+	testCases := []struct {
+		name    string
+		request *rollupv1.InitiateWithdrawalRequest
+		errMsg  string
+	}{
+		{
+			name: "Valid request",
+			request: &rollupv1.InitiateWithdrawalRequest{
+				Target:   validAddress,
+				GasLimit: validGasLimit,
+			},
+			errMsg: "",
+		},
+		{
+			name: "Invalid Ethereum address",
+			request: &rollupv1.InitiateWithdrawalRequest{
+				Target:   invalidAddress,
+				GasLimit: validGasLimit,
+			},
+			errMsg: "invalid Ethereum address",
+		},
+		{
+			name: "Gas limit below the allowed range",
+			request: &rollupv1.InitiateWithdrawalRequest{
+				Target:   validAddress,
+				GasLimit: belowRangeGasLimit,
+			},
+			errMsg: "gas limit must be between 5,000 and 9,223,372,036,854,775,807",
+		},
+		{
+			name: "Gas limit above the allowed range",
+			request: &rollupv1.InitiateWithdrawalRequest{
+				Target:   validAddress,
+				GasLimit: aboveRangeGasLimit,
+			},
+			errMsg: "gas limit must be between 5,000 and 9,223,372,036,854,775,807",
+		},
 	}
 
-	err := validateBasic(validRequest)
-	require.NoError(t, err)
-
-	// Invalid Ethereum address
-	invalidAddressRequest := &rollupv1.InitiateWithdrawalRequest{
-		Target:   invalidAddress,
-		GasLimit: validGasLimit,
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			err := validateBasic(tc.request)
+			if tc.errMsg == "" {
+				require.NoError(t, err)
+			} else {
+				require.Error(t, err)
+				require.Contains(t, err.Error(), tc.errMsg)
+			}
+		})
 	}
-
-	err = validateBasic(invalidAddressRequest)
-	require.Error(t, err)
-	require.Contains(t, err.Error(), "invalid Ethereum address")
-
-	// Gas limit below the allowed range
-	lowGasLimitRequest := &rollupv1.InitiateWithdrawalRequest{
-		Target:   validAddress,
-		GasLimit: belowRangeGasLimit,
-	}
-
-	err = validateBasic(lowGasLimitRequest)
-	require.Error(t, err)
-	require.Contains(t, err.Error(), "gas limit must be between 5,000 and 9,223,372,036,854,775,807")
-
-	// Gas limit above the allowed range
-	highGasLimitRequest := &rollupv1.InitiateWithdrawalRequest{
-		Target:   validAddress,
-		GasLimit: aboveRangeGasLimit,
-	}
-	err = validateBasic(highGasLimitRequest)
-	require.Error(t, err)
-	require.Contains(t, err.Error(), "gas limit must be between 5,000 and 9,223,372,036,854,775,807")
 }
