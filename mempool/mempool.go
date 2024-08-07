9a4193ba9d35a4dc074ad02c14c65b563cc6c788
AnkushinDaniil <ankushin.daniil42@gmail.com>
2024-08-07 16:56:14 +0300

    Change len to err check

--- mempool/mempool.go
+++ mempool/mempool.go
@@ -41,8 +41,7 @@ func (p *Pool) Enqueue(userTxn comettypes.Tx) error {
 	// Attempt to adapt the Cosmos transaction to an Ethereum deposit transaction.
 	// If the adaptation results in one or more transactions, it indicates that the
 	// user transaction is a deposit transaction, which is not allowed in the pool.
-	if txs, _ := rolluptypes.AdaptCosmosDepositTxToEthTx(userTxn); len(txs) > 0 {
-		// Return an error indicating that deposit transactions are not allowed in the pool.
+	if _, err := rolluptypes.AdaptCosmosDepositTxToEthTx(userTxn); err == nil {
 		return errors.New("deposit txs are not allowed in the pool")
 	}
 
