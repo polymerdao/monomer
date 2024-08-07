5953934acf21a358938ead651d1839049f2a88d1
AnkushinDaniil <ankushin.daniil42@gmail.com>
2024-08-07 18:24:33 +0300

    Change params to local constants in error msg

--- x/rollup/keeper/msg_server.go
+++ x/rollup/keeper/msg_server.go
@@ -245,7 +245,7 @@ func ValidateBasic(m *rollupv1.InitiateWithdrawalRequest) error {
 	// Check if the gas limit is within the allowed range.
 	gasLimit := new(big.Int).SetBytes(m.GasLimit).Uint64()
 	if gasLimit < MinTxGasLimit || gasLimit > MaxTxGasLimit {
-		return fmt.Errorf("gas limit must be between %d and %d: %d", params.TxGas, params.MaxGasLimit, gasLimit)
+		return fmt.Errorf("gas limit must be between %d and %d: %d", MinTxGasLimit, MinTxGasLimit, gasLimit)
 	}
 
 	return nil
