ebb4d945e3284b849a4c1f9141d0bdeb03a4ffa0
AnkushinDaniil <ankushin.daniil42@gmail.com>
2024-08-09 17:57:16 +0300

    Move wrapCloseErr to utils

--- utils/utils.go
+++ utils/utils.go
@@ -1,5 +1,23 @@
 package utils
 
+import (
+	"fmt"
+	"io"
+
+	"github.com/hashicorp/go-multierror"
+)
+
 func Ptr[T any](x T) *T {
 	return &x
+}
+
+func WrapCloseErr(err error, closer io.Closer) error {
+	closeErr := closer.Close()
+	if closeErr != nil {
+		closeErr = fmt.Errorf("close: %v", closeErr)
+	}
+	if err != nil || closeErr != nil {
+		return multierror.Append(err, closeErr)
+	}
+	return nil
 }
