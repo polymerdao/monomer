fdd593822a67eb0e5dfe261cca1ef21d3582c53c
Colin Kennedy <colin.kennedy@mesh.xyz>
2024-08-09 11:13:04 -0300

    Sign deposit Tx inline

--- x/rollup/types/adapters.go
+++ x/rollup/types/adapters.go
@@ -1,15 +1,23 @@
 package types
 
 import (
+	"context"
 	"errors"
 	"fmt"
+	"runtime"
 
 	bfttypes "github.com/cometbft/cometbft/types"
+	cosmosclient "github.com/cosmos/cosmos-sdk/client"
+	cosmostx "github.com/cosmos/cosmos-sdk/client/tx"
 	codectypes "github.com/cosmos/cosmos-sdk/codec/types"
+	"github.com/cosmos/cosmos-sdk/crypto/keys/ed25519"
+	sdktypes "github.com/cosmos/cosmos-sdk/types"
 	sdktx "github.com/cosmos/cosmos-sdk/types/tx"
+	"github.com/cosmos/cosmos-sdk/types/tx/signing"
+	authsigning "github.com/cosmos/cosmos-sdk/x/auth/signing"
 	"github.com/ethereum/go-ethereum/common/hexutil"
 	ethtypes "github.com/ethereum/go-ethereum/core/types"
-	"github.com/polymerdao/monomer/gen/rollup/v1"
+	rollupv1 "github.com/polymerdao/monomer/gen/rollup/v1"
 )
 
 var errL1AttributesNotFound = errors.New("L1 attributes tx not found")
@@ -48,11 +56,93 @@ func AdaptPayloadTxsToCosmosTxs(ethTxs []hexutil.Bytes, appchainCtx cosmosclient
 	if err != nil {
 		return nil, fmt.Errorf("new any with value: %v", err)
 	}
-	depositSDKMsgBytes, err := (&sdktx.Tx{
+	depositTx := sdktx.Tx{
 		Body: &sdktx.TxBody{
 			Messages: []*codectypes.Any{msgAny},
 		},
-	}).Marshal()
+	}
+
+	privKey := ed25519.GenPrivKeyFromSecret([]byte("monomer"))
+	pubKey := privKey.PubKey()
+	address := pubKey.Address()
+
+	txConfig := appchainCtx.TxConfig
+	txBuilder := txConfig.NewTxBuilder()
+
+	acc, err := appchainCtx.AccountRetriever.GetAccount(appchainCtx, sdktypes.AccAddress(address.Bytes()))
+
+	if err != nil {
+		return nil, fmt.Errorf("get account: %v", err)
+	}
+
+	signerData := authsigning.SignerData{
+		ChainID:       appchainCtx.ChainID,
+		AccountNumber: acc.GetAccountNumber(),
+		Sequence:      acc.GetSequence(),
+		PubKey:        pubKey,
+		Address:       acc.GetAddress().String(),
+	}
+
+	blankSig := signing.SignatureV2{
+		PubKey:   pubKey,
+		Sequence: acc.GetSequence(),
+		Data: &signing.SingleSignatureData{
+			SignMode:  signing.SignMode_SIGN_MODE_DIRECT,
+			Signature: nil,
+		},
+	}
+
+	err = txBuilder.SetMsgs(msgAny)
+	if err != nil {
+		return nil, fmt.Errorf("set msgs: %v", err)
+	}
+	err = txBuilder.SetSignatures(blankSig)
+	if err != nil {
+		return nil, fmt.Errorf("set signatures: %v", err)
+	}
+
+	sig, err := cosmostx.SignWithPrivKey(
+		context.Background(),
+		signing.SignMode_SIGN_MODE_DIRECT,
+		signerData,
+		txBuilder,
+		privKey,
+		txConfig,
+		acc.GetSequence(),
+	)
+	if err != nil {
+		return nil, fmt.Errorf("sign with priv key: %v", err)
+	}
+	err = txBuilder.SetSignatures(sig)
+	if err != nil {
+		return nil, fmt.Errorf("set signatures: %v", err)
+	}
+	// tx := txBuilder.GetTx()
+
+	pubKeyAny, err := codectypes.NewAnyWithValue(pubKey)
+	if err != nil {
+		return nil, fmt.Errorf("new any with value: %v", err)
+	}
+
+	depositTx.AuthInfo = &sdktx.AuthInfo{
+		SignerInfos: []*sdktx.SignerInfo{
+			{
+				PublicKey: pubKeyAny,
+				ModeInfo: &sdktx.ModeInfo{
+					// Assuming you want single signer mode
+					Sum: &sdktx.ModeInfo_Single_{
+						Single: &sdktx.ModeInfo_Single{
+							Mode: signing.SignMode_SIGN_MODE_DIRECT,
+						},
+					},
+				},
+				Sequence: acc.GetSequence(),
+			},
+		},
+	}
+	depositTx.Signatures = [][]byte{sig.Data.(*signing.SingleSignatureData).Signature}
+
+	depositSDKMsgBytes, err := depositTx.Marshal()
 	if err != nil {
 		return nil, fmt.Errorf("marshal tx: %v", err)
 	}
