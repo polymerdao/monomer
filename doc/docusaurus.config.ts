8ab2cef3a328747b8739e348ca31e345ceab5e25
AnkushinDaniil <ankushin.daniil42@gmail.com>
2024-08-19 19:16:01 +0300

    Add routeBasePath in docusaurus.config.ts

--- doc/docusaurus.config.ts
+++ doc/docusaurus.config.ts
@@ -35,6 +35,7 @@ const config: Config = {
       'classic',
       {
         docs: {
+          routeBasePath: '/',
           sidebarPath: './sidebars.ts',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
