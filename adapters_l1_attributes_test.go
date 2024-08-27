bf6050ad5cbf01ee86d3378879ac5e62e960665c
AnkushinDaniil <ankushin.daniil42@gmail.com>
2024-08-27 19:33:13 +0300

    Delete experimental code

--- adapters_l1_attributes_test.go
+++ adapters_l1_attributes_test.go
@@ -1,13 +1,9 @@
 package monomer_test
 
 import (
-	"bytes"
-	"fmt"
-	"log"
 	"testing"
 
 	"github.com/ethereum-optimism/optimism/op-node/rollup/derive"
-	"github.com/ethereum/go-ethereum/accounts/abi"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/ethereum/go-ethereum/core/types"
 	"github.com/polymerdao/monomer"
@@ -105,43 +101,5 @@ func TestIsL1AttributesTx(t *testing.T) {
 		t.Run(test.name, func(t *testing.T) {
 			require.Equal(t, test.want, monomer.IsL1AttributesTx(&test.tx))
 		})
-	}
-}
-
-func TestABI(t *testing.T) {
-	// The ABI of the L1Block contract (replace with the actual ABI)
-	const l1BlockABI = `[
-      {
-       "inputs": [],
-       "name": "setL1BlockValuesEcotone",
-       "outputs": [],
-       "stateMutability": "nonpayable",
-       "type": "function"
-      }
-     ],`
-
-	// Example transaction data (replace with actual data)
-	// txData := common.Hex2Bytes(string(tx.Data()))
-
-	// Parse the ABI
-	parsedABI, err := abi.JSON(bytes.NewReader([]byte(l1BlockABI)))
-	if err != nil {
-		log.Fatalf("Failed to parse ABI: %v", err)
-	}
-
-	fmt.Printf("%+v\n\n\n", parsedABI)
-
-	// Get the method by name
-	method := parsedABI.Methods["setL1BlockValuesEcotone"]
-
-	fmt.Printf("%+v\n", method)
-	methodID := method.ID
-	fmt.Println(bytes.Equal(methodID, []byte{68, 10, 94, 32}))
-
-	invalidTxData := []byte{0x01, 0x02, 0x03, 0x04}
-
-	// Check if the transaction data starts with the expected method ID
-	if len(invalidTxData) < len(methodID) || !bytes.Equal(invalidTxData[:len(methodID)], methodID) {
-		fmt.Println("Data is NOT a call to the setL1BlockValues function.")
 	}
 }
