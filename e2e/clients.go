5120a4282c4e3a4b36db324ada23932d5028ceba
Nate Beauregard <nathanbeauregard@gmail.com>
2024-09-09 12:18:28 -0400

    Add MonomerClient GetProof implementation

--- e2e/clients.go
+++ e2e/clients.go
@@ -8,6 +8,7 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 	ethtypes "github.com/ethereum/go-ethereum/core/types"
 	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/ethereum/go-ethereum/ethclient/gethclient"
 	"github.com/ethereum/go-ethereum/rpc"
 )
 
@@ -25,14 +26,16 @@ func NewL1Client(client *rpc.Client) *L1Client {
 
 type MonomerClient struct {
 	client *rpc.Client
-	// We don't embed the ethclient.Client struct because Monomer doesn't implement the full `eth_*` interface.
-	ethclient *ethclient.Client
+	// We don't embed the ethclient.Client and gethclient.Client structs because Monomer doesn't implement the full geth `eth_*` interface.
+	ethclient  *ethclient.Client
+	gethclient *gethclient.Client
 }
 
 func NewMonomerClient(client *rpc.Client) *MonomerClient {
 	return &MonomerClient{
-		client:    client,
-		ethclient: ethclient.NewClient(client),
+		client:     client,
+		ethclient:  ethclient.NewClient(client),
+		gethclient: gethclient.New(client),
 	}
 }
 
@@ -53,4 +56,19 @@ func (m *MonomerClient) BlockByNumber(ctx context.Context, number *big.Int) (*et
 		return nil, err
 	}
 	return block, nil
+}
+
+// GetProof returns the account and storage values of the specified account including the Merkle-proof.
+// The block number can be nil, in which case the value is taken from the latest known block.
+func (m *MonomerClient) GetProof(
+	ctx context.Context,
+	account common.Address,
+	keys []string,
+	blockNumber *big.Int,
+) (*gethclient.AccountResult, error) {
+	accountResult, err := m.gethclient.GetProof(ctx, account, keys, blockNumber)
+	if err != nil {
+		return nil, fmt.Errorf("get proof: %v", err)
+	}
+	return accountResult, nil
 }
