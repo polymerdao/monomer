b12a8a35db0157dbf565e2b6495aa69bc37d170b
Nate Beauregard <nathanbeauregard@gmail.com>
2024-09-09 12:12:04 -0400

    Refactor monomer eth_getProof
    - Moves copied types to eth/internal/api.go
    - Removes client implementation for eth_getProof
    - Adds functionality to support getting proofs
      by block hash

--- eth/internal/ethapi/api.go
+++ eth/internal/ethapi/api.go
@@ -10,6 +10,23 @@ import (
 	"github.com/ethereum/go-ethereum/params"
 )
 
+// AccountResult structs for GetProof
+type AccountResult struct {
+	Address      common.Address  `json:"address"`
+	AccountProof []string        `json:"accountProof"`
+	Balance      *hexutil.Big    `json:"balance"`
+	CodeHash     common.Hash     `json:"codeHash"`
+	Nonce        hexutil.Uint64  `json:"nonce"`
+	StorageHash  common.Hash     `json:"storageHash"`
+	StorageProof []StorageResult `json:"storageProof"`
+}
+
+type StorageResult struct {
+	Key   string       `json:"key"`
+	Value *hexutil.Big `json:"value"`
+	Proof []string     `json:"proof"`
+}
+
 // RPCTransaction represents a transaction that will serialize to the RPC representation of a transaction
 type RPCTransaction struct {
 	BlockHash           *common.Hash      `json:"blockHash"`
