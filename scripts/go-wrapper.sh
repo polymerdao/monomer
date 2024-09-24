f353576df023463e4fc7b242f66152a212c31406
AnkushinDaniil <ankushin.daniil42@gmail.com>
2024-09-24 12:43:44 +0300

    Update

--- scripts/go-wrapper.sh
+++ scripts/go-wrapper.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+set -e
 # Because we transitively depend on github.com/fjl/memsize, we need to disable checklinkname in go1.23.0 and higher.
 goVersion=$(go env GOVERSION)
 if [[ $goVersion > go1.23.0 || $goVersion == go1.23.0 ]]; then
