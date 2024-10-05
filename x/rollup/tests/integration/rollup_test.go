f4de952d60c67089894ec37c45194950528cccbf
Josh Klopfenstein <git@joshklop.com>
2024-10-05 10:54:16 -0500

    Fix dummy signer

--- x/rollup/tests/integration/rollup_test.go
+++ x/rollup/tests/integration/rollup_test.go
@@ -141,6 +141,7 @@ func setupIntegrationApp(t *testing.T) *integration.App {
 		encodingCfg.Codec,
 		runtime.NewKVStoreService(keys[rolluptypes.StoreKey]),
 		bankKeeper,
+		accountKeeper,
 	)
 
 	authModule := auth.NewAppModule(encodingCfg.Codec, accountKeeper, authsims.RandomGenesisAccounts, nil)
