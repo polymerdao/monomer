2554d571b130435a5d38287a769468899c87938e
Nate Beauregard <nathanbeauregard@gmail.com>
2024-10-08 11:33:36 -0400

    Expose gRPC and API server
    Expose the Cosmos gRPC and API servers when starting
    a monomer node in process through the Monomer start
    command.

--- integrations/integrations.go
+++ integrations/integrations.go
@@ -8,17 +8,22 @@ import (
 	"io"
 	"net"
 	"os"
+	"os/signal"
 	"path/filepath"
 	"strconv"
 	"strings"
+	"syscall"
 
 	"github.com/cockroachdb/pebble"
 	"github.com/cockroachdb/pebble/vfs"
 	cometdb "github.com/cometbft/cometbft-db"
 	dbm "github.com/cosmos/cosmos-db"
 	"github.com/cosmos/cosmos-sdk/client"
+	"github.com/cosmos/cosmos-sdk/codec"
 	"github.com/cosmos/cosmos-sdk/server"
+	"github.com/cosmos/cosmos-sdk/server/api"
 	serverconfig "github.com/cosmos/cosmos-sdk/server/config"
+	servergrpc "github.com/cosmos/cosmos-sdk/server/grpc"
 	servertypes "github.com/cosmos/cosmos-sdk/server/types"
 	genutiltypes "github.com/cosmos/cosmos-sdk/x/genutil/types"
 	"github.com/ethereum/go-ethereum/core/rawdb"
@@ -29,8 +34,11 @@ import (
 	"github.com/polymerdao/monomer/genesis"
 	"github.com/polymerdao/monomer/monomerdb/localdb"
 	"github.com/polymerdao/monomer/node"
+	"github.com/polymerdao/monomer/utils"
 	"github.com/spf13/viper"
 	"golang.org/x/sync/errgroup"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
 )
 
 const (
@@ -75,16 +83,17 @@ func StartCommandHandler(
 		return fmt.Errorf("start application: %v", err)
 	}
 
-	monomerCtx, cancel := context.WithCancel(context.Background())
-	defer cancel()
+	g, monomerCtx := getCtx(svrCtx)
 
 	// Would usually start a Comet node in-process here, but we replace the
 	// Comet node with a Monomer node.
-	if err := startInProcess(env, svrCtx, &clientCtx, monomerCtx, app, opts); err != nil {
+	if err = startInProcess(env, g, svrCtx, &svrCfg, &clientCtx, monomerCtx, app, opts); err != nil {
 		return fmt.Errorf("start Monomer node in-process: %v", err)
 	}
 
-	<-sigCh
+	if err = g.Wait(); err != nil {
+		return fmt.Errorf("unexpected error in errgroup: %v", err)
+	}
 
 	return nil
 }
@@ -125,7 +134,9 @@ func startApp(
 // We modify this function to start a Monomer node in-process instead of a Comet node.
 func startInProcess(
 	env *environment.Env,
+	g *errgroup.Group,
 	svrCtx *server.Context,
+	svrCfg *serverconfig.Config,
 	clientCtx *client.Context,
 	monomerCtx context.Context,
 	app servertypes.Application,
@@ -139,13 +150,23 @@ func startInProcess(
 		return fmt.Errorf("start Monomer node: %v", err)
 	}
 
+	// Add the tx service to the gRPC router if API or gRPC is enabled.
+	if svrCfg.API.Enable || svrCfg.GRPC.Enable {
+		app.RegisterTxService(*clientCtx)
+		app.RegisterTendermintService(*clientCtx)
+		app.RegisterNodeService(*clientCtx, *svrCfg)
+	}
+
+	grpcSrv, clientCtx, err := startGrpcServer(monomerCtx, g, svrCfg.GRPC, clientCtx, svrCtx, app)
+	if err != nil {
+		return fmt.Errorf("start gRPC server: %v", err)
+	}
+
+	startAPIServer(monomerCtx, g, svrCfg, clientCtx, svrCtx, app, svrCtx.Config.RootDir, grpcSrv)
+
 	if opts.PostSetup != nil {
-		var g errgroup.Group
-		if err := opts.PostSetup(svrCtx, *clientCtx, monomerCtx, &g); err != nil {
+		if err = opts.PostSetup(svrCtx, *clientCtx, monomerCtx, g); err != nil {
 			return fmt.Errorf("run post setup: %v", err)
-		}
-		if err := g.Wait(); err != nil {
-			return fmt.Errorf("unexpected error in PostSetup errgroup: %v", err)
 		}
 	}
 
@@ -272,4 +293,105 @@ func startMonomerNode(
 	svrCtx.Logger.Info("Monomer started w/ CometBFT listener on", "address", cometListener.Addr())
 
 	return nil
+}
+
+// Starts the gRPC server if enabled in the server configuration.
+func startGrpcServer(
+	monomerCtx context.Context,
+	g *errgroup.Group,
+	config serverconfig.GRPCConfig,
+	clientCtx *client.Context,
+	svrCtx *server.Context,
+	app servertypes.Application,
+) (*grpc.Server, *client.Context, error) {
+	if !config.Enable {
+		// return grpcServer as nil if gRPC is disabled
+		return nil, clientCtx, nil
+	}
+	_, _, err := net.SplitHostPort(config.Address)
+	if err != nil {
+		return nil, clientCtx, fmt.Errorf("invalid gRPC server address: %v", err)
+	}
+
+	maxSendMsgSize := config.MaxSendMsgSize
+	if maxSendMsgSize == 0 {
+		maxSendMsgSize = serverconfig.DefaultGRPCMaxSendMsgSize
+	}
+
+	maxRecvMsgSize := config.MaxRecvMsgSize
+	if maxRecvMsgSize == 0 {
+		maxRecvMsgSize = serverconfig.DefaultGRPCMaxRecvMsgSize
+	}
+
+	// if gRPC is enabled, configure gRPC client for gRPC gateway
+	grpcClient, err := grpc.Dial( //nolint: staticcheck // ignore this line for this linter
+		config.Address,
+		grpc.WithTransportCredentials(insecure.NewCredentials()),
+		grpc.WithDefaultCallOptions(
+			grpc.ForceCodec(codec.NewProtoCodec(clientCtx.InterfaceRegistry).GRPCCodec()),
+			grpc.MaxCallRecvMsgSize(maxRecvMsgSize),
+			grpc.MaxCallSendMsgSize(maxSendMsgSize),
+		),
+	)
+	if err != nil {
+		return nil, clientCtx, fmt.Errorf("failed to dial gRPC server: %v", err)
+	}
+
+	clientCtx = utils.Ptr(clientCtx.WithGRPCClient(grpcClient))
+
+	grpcSrv, err := servergrpc.NewGRPCServer(*clientCtx, app, config)
+	if err != nil {
+		return nil, clientCtx, fmt.Errorf("failed to create gRPC server: %v", err)
+	}
+
+	// Start the gRPC server in a goroutine. Note, the provided ctx will ensure
+	// that the server is gracefully shut down.
+	g.Go(func() error {
+		return servergrpc.StartGRPCServer(monomerCtx, svrCtx.Logger.With("module", "grpc-server"), config, grpcSrv)
+	})
+	return grpcSrv, clientCtx, nil
+}
+
+// Starts the API server if enabled in the server configuration.
+func startAPIServer(
+	monomerCtx context.Context,
+	g *errgroup.Group,
+	svrCfg *serverconfig.Config,
+	clientCtx *client.Context,
+	svrCtx *server.Context,
+	app servertypes.Application,
+	home string,
+	grpcSrv *grpc.Server,
+) {
+	if svrCfg.API.Enable {
+		clientCtx = utils.Ptr(clientCtx.WithHomeDir(home))
+
+		apiSrv := api.New(*clientCtx, svrCtx.Logger.With("module", "api-server"), grpcSrv)
+		app.RegisterAPIRoutes(apiSrv, svrCfg.API)
+
+		// Start the API server in a goroutine. Note, the provided ctx will ensure
+		// that the server is gracefully shut down.
+		g.Go(func() error {
+			return apiSrv.Start(monomerCtx, *svrCfg)
+		})
+	}
+}
+
+// getCtx returns a new errgroup and context for the Monomer node.
+func getCtx(svrCtx *server.Context) (*errgroup.Group, context.Context) {
+	ctx, cancelFn := context.WithCancel(context.Background())
+	g, ctx := errgroup.WithContext(ctx)
+
+	// Listen for SIGINT and SIGTERM quit signals. When a signal is received,
+	// the cleanup function is called, indicating the caller can gracefully exit or
+	// return.
+	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
+	g.Go(func() error {
+		sig := <-sigCh
+		svrCtx.Logger.Info("Shutting down Monomer...", "signal", sig.String())
+		cancelFn()
+		return nil
+	})
+
+	return g, ctx
 }
