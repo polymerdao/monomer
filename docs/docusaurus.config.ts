3b02b90cbd35a911500112d8d19aa0bf285def06
Nate Beauregard <nathanbeauregard@gmail.com>
2024-10-18 10:32:36 -0700

    Remove custom module section and fix edit page link

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -39,7 +39,7 @@ const config: Config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/polymerdao/monomer/tree/main/doc/',
+            'https://github.com/polymerdao/monomer/tree/main/docs/',
         },
         // Do not include a blog
         blog: false,
