64b7a4cb2925036229e49ab488e5923d788541f7
Josh Klopfenstein <git@joshklop.com>
2024-10-22 17:54:18 -0700

    Make go-wrapper.sh work with all go commands

--- scripts/go-wrapper.sh
+++ scripts/go-wrapper.sh
@@ -3,7 +3,8 @@ set -e
 # Because we transitively depend on github.com/fjl/memsize, we need to disable checklinkname in go1.23.0 and higher.
 goVersion=$(go env GOVERSION)
 if [[ $goVersion > go1.23.0 || $goVersion == go1.23.0 ]]; then
-    exec go "$@" -ldflags=-checklinkname=0
-else
-    exec go "$@"
+    LDFLAGS+=' -checklinkname=0 '
 fi
+
+export LDFLAGS
+go "$@"
