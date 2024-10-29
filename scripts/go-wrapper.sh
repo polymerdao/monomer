2025f04d7edb4dc1f60540ad9d7bef7a8629d6a9
Josh Klopfenstein <git@joshklop.com>
2024-10-29 13:32:53 -0500

    Use correct env var for go wrapper (#283)

--- scripts/go-wrapper.sh
+++ scripts/go-wrapper.sh
@@ -3,8 +3,8 @@ set -e
 # Because we transitively depend on github.com/fjl/memsize, we need to disable checklinkname in go1.23.0 and higher.
 goVersion=$(go env GOVERSION)
 if [[ $goVersion > go1.23.0 || $goVersion == go1.23.0 ]]; then
-    LDFLAGS+=' -checklinkname=0 '
+    GOFLAGS+=' -ldflags=-checklinkname=0 '
 fi
 
-export LDFLAGS
+export GOFLAGS
 go "$@"
